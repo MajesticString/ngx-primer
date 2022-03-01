6bacdabf4ab91eb9732b8d8f6993224af5f62162
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-02-28 18:13:59 -0800

    Move folder into src

--- projects/ngx-primer/src/public-api.ts
+++ projects/ngx-primer/src/public-api.ts
@@ -2,10 +2,10 @@
  * Public API Surface of ngx-primer
  */
 
-export * from '../box/box.module';
-export * from '../box/box.component';
-export * from '../box/box-body/box-body.component';
-export * from '../box/box-footer/box-footer.component';
-export * from '../box/box-header/box-header.component';
-export * from '../box/box-row/box-row.component';
-export * from '../box/box-title/box-title.component';
+export * from "./box/box.module";
+export * from "./box/box.component";
+export * from "./box/box-body/box-body.component";
+export * from "./box/box-footer/box-footer.component";
+export * from "./box/box-header/box-header.component";
+export * from "./box/box-row/box-row.component";
+export * from "./box/box-title/box-title.component";
